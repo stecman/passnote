302c5ab44babfed422e8e193a373532763900e46
Stephen Holdaway <stecman@gmail.com>
2015-03-01 09:26:50 +1300

    Update base schema to be working version
    For some reason the committed base schema was quite out of date. The application
    would have been difficult to get going with the previous file.

--- docs/mysql/schema.sql
+++ docs/mysql/schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: passnote
 -- ------------------------------------------------------
--- Server version	5.5.37-1
+-- Server version	5.5.41-0ubuntu0.14.04.1-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,13 +24,15 @@ DROP TABLE IF EXISTS `key`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `key` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
   `name` varchar(100) NOT NULL DEFAULT '',
   `public_key` blob NOT NULL,
   `private_key` blob NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `user_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `fk_key_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,18 +43,46 @@ DROP TABLE IF EXISTS `object`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `object` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `created` datetime NOT NULL,
   `title` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `content` longblob,
-  `key` blob,
-  `key_id` int(11) DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
+  `content` longblob COMMENT 'The content of the object. Encrypted.',
+  `format` varchar(45) DEFAULT NULL COMMENT 'Identifier for the formatter to render the object with.',
+  `checksum` tinyblob NOT NULL COMMENT 'The SHA1 checksum of the unencrypted value of `content`.',
+  `encryptionKey` blob NOT NULL,
+  `encryptionKeyIv` blob NOT NULL,
+  `key_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
   `parent_id` int(11) DEFAULT NULL,
-  `isBinary` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `key_id` (`key_id`),
+  CONSTRAINT `fk_object_key_id` FOREIGN KEY (`key_id`) REFERENCES `key` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_object_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `object_version`
+--
+
+DROP TABLE IF EXISTS `object_version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `object_version` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
+  `object_id` int(10) unsigned NOT NULL,
+  `content` longblob,
+  `format` varchar(45) DEFAULT NULL,
+  `encryptionKey` blob NOT NULL,
+  `encryptionKeyIv` blob NOT NULL,
+  `checksum` tinyblob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `object_id` (`object_id`),
+  CONSTRAINT `fk_object_version_object_id` FOREIGN KEY (`object_id`) REFERENCES `object` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,17 +94,18 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
   `email` varchar(50) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `otpKey` blob,
   `otpIv` blob,
-  `accountKey_id` int(11) DEFAULT NULL,
+  `accountKey_id` int(11) unsigned DEFAULT NULL,
   `sessionKey` tinyblob NOT NULL,
   `accountKeyPhrase` blob,
   `accountKeyIv` blob,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -86,4 +117,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-05-13  9:15:03
+-- Dump completed on 2015-02-28 14:21:16
