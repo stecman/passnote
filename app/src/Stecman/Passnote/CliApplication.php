77955d0eb5990d43c91b0040de72501e7e88856f
Stephen Holdaway <stecman@gmail.com>
2019-03-15 10:37:02 +1300

    Add console interface for adding new data

---
+++ app/src/Stecman/Passnote/CliApplication.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Stecman\Passnote;
+
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Helper\HelperSet;
+
+class CliApplication extends BaseApplication
+{
+    /**
+     * Set the name and version (with the version a dummy var to be swapped out by the compiler)
+     */
+    public function __construct()
+    {
+        parent::__construct('Passnote', 'v3.0');
+    }
+
+    protected function getDefaultCommands()
+    {
+        $commands = parent::getDefaultCommands();
+
+        $commands[] = new Command\AddCommand();
+
+        return $commands;
+    }
+
+    /**
+     * Return the default helper set
+     * @return \Symfony\Component\Console\Helper\HelperSet
+     */
+    protected function getDefaultHelperSet()
+    {
+        return new HelperSet();
+    }
+}
